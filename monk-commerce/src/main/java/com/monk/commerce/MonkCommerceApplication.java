8a63807e4cc64355c3c756498bee01b4b36208c0
infx012685 <dipali.gangawane@infinx.com>
2024-11-22 16:49:15 +0530

    Initial Project Setup

---
+++ monk-commerce/src/main/java/com/monk/commerce/MonkCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.monk.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkCommerceApplication.class, args);
+	}
+
+}
