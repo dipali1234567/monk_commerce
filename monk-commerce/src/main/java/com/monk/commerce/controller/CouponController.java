c6bee4085a62d9804b9f5e078ff8ef4730b7da0e
infx012685 <dipali.gangawane@infinx.com>
2024-11-22 18:18:52 +0530

    Added Dummy API's

--- monk-commerce/src/main/java/com/monk/commerce/controller/CouponController.java
+++ monk-commerce/src/main/java/com/monk/commerce/controller/CouponController.java
@@ -1,9 +1,34 @@
 package com.monk.commerce.controller;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/coupons")
 public class CouponController {
 
+    @PostMapping(value="/")
+    public ResponseEntity<?> createCoupon(@RequestBody Object coupon) {
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping(value="/{couponId}")
+    public ResponseEntity<?> updateCoupon(@RequestBody Object coupon, @PathVariable(value = "couponId") int id) {
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/{couponId}")
+    public ResponseEntity<?> deleteCoupon( @PathVariable(value = "couponId") int id) {
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping(value="/{couponId}")
+    public ResponseEntity<?> getCoupon( @PathVariable(value = "couponId") int id) {
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping(value="/")
+    public ResponseEntity<?> getCoupons() {
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
 }
